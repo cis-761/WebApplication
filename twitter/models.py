6d4f89389acaa033328c6b576f9d83ed75648152
Luis Bobadilla <happystep@ksu.edu>
2019-04-24 12:30:00 -0500

    added models for Flu and Symptoms

--- twitter/models.py
+++ twitter/models.py
@@ -3,20 +3,23 @@ from django.conf import settings
 from django.utils import timezone
 
 class Tweets(models.Model):
-    # id - implicit, not null, primary key
     text = models.CharField(max_length=30)
     location = models.CharField(max_length=30)
     favorite = models.BooleanField(default=False)
     date = models.DateTimeField
 
 class ReTweets(models.Model):
-     # id - implicit, not null, primary key
-      text = models.CharField(max_length=300) # double check what the current character count limit for tweets are 
+    text = models.CharField(max_length=300) 
 
 class User(models.Model):
-     # id - implicit, not null, primary key
-     name = models.CharField(max_length=30)
-     screen_name = models.CharField(max_length=30)
-     geo_enabled = models.BooleanField(default=False)
-     verified = models.BooleanField(default=False)
-     
+    name = models.CharField(max_length=30)
+    screen_name = models.CharField(max_length=30)
+    geo_enabled = models.BooleanField(default=False)
+    verified = models.BooleanField(default=False)
+
+class Flu(models.Model):
+    flu_type = models.CharField(max_length=30)
+
+class Symptoms(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=60)
