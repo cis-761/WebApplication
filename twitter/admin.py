4afb200dc9c30b98206219b4b85933119dbb656d
Luis Bobadilla <happystep@ksu.edu>
2019-05-04 11:05:09 -0500

    registered new models

--- twitter/admin.py
+++ twitter/admin.py
@@ -6,8 +6,22 @@ from .models import Tweets
 from .models import User
 from .models import Flu
 from .models import Symptoms
+from .models import Trends
+from .models import fluSymptoms
+from .models import tweetFlu
+from .models import userTweets
+from .models import tweetFlu
+from .models import userTweets
+from .models import tweetTrends
 
 admin.site.register(Tweets)
 admin.site.register(User)
 admin.site.register(Flu)
 admin.site.register(Symptoms)
+admin.site.register(Trends)
+admin.site.register(fluSymptoms)
+admin.site.register(tweetFlu)
+admin.site.register(userTweets)
+admin.site.register(tweetFlu)
+admin.site.register(userTweets)
+admin.site.register(tweetTrends)
