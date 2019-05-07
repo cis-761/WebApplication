8d0bd019dcc1c62e669068bbc7c50b6faaed7864
Luis Bobadilla <happystep@ksu.edu>
2019-05-07 16:29:42 -0500

    started working on login and register and it didn't work, decided we will use admin for creating accounts for other users

--- twitter/urls.py
+++ twitter/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from django.conf.urls import url, include
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('', views.tweets_list, name='tweets_list'),
@@ -7,3 +10,9 @@ urlpatterns = [
     path('results/', views.results, name='results'),
 ]
 
+normalpatterns = [
+        url(r'^login/', auth_views.LoginView,{'template_name': 'login.html'}, name ='login'),
+         url(r'^register/', views.register,name ='register' ),
+]
+
+urlpatterns += normalpatterns
--- twitter/views.py
+++ twitter/views.py
@@ -16,6 +16,9 @@ import re
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 from matplotlib.dates import DateFormatter
+from django.contrib.auth import login as auth_login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -132,3 +135,26 @@ def word_in_text(word, text):
     if match:
         return True
     return False
+
+def login(request):
+    if request.POST:
+        if 'login' in request.POST:
+            return HttpResponseRedirect('/youloggedin/')
+        else:
+            return HttpResponseRedirect('/youregistered')
+    else:
+        return render(request, '/twitter/login.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            auth_login(request, user)
+            return redirect('/')
+    else:
+        form = UserCreationForm()
+    return render(request, '/twitter/register.html', {'form': form})
